906eb501090682ed51545e11fd0bef52ab77ba29
Ilia_m <neonfoxik1@gmail.com>
2025-02-08 21:07:41 +0300

    commit

---
+++ bot/admin.py
@@ -0,0 +1,73 @@
+from django.contrib import admin
+from .models import (
+    User,
+    Mode,
+    Prompt,
+    Transaction,
+    UserMode,
+    TrainingMaterial
+)
+
+
+class ModeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'model', 'max_token', ]
+    list_display_links = ['name']
+    search_fields = ['name', 'model', ]
+    ordering = ['max_token', ]
+
+
+class PromptAdmin(admin.ModelAdmin):
+    list_display = ['text', 'name', ]
+    list_display_links = ['name', ]
+    search_fields = ['name', ]
+
+
+class ReferalAdmin(admin.ModelAdmin):
+    list_display = ['inviter', 'is_used', ]
+    search_fields = ['inviter', ]
+    ordering = ['is_used', ]
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['telegram_id', 'name', 'has_plan', 'plan_end', ]
+    list_display_links = ['telegram_id', ]
+    search_fields = ['telegram_id', 'name', 'has_plan']
+    list_editable = ['has_plan',]
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'cash', ]
+    list_display_links = ['user', ]
+    search_fields = ['user', 'cash', ]
+
+
+class UserPlanAdmin(admin.ModelAdmin):
+    list_display = ['user', 'plan', 'renew_date']
+    list_display_links = ['user', ]
+    search_fields = ['user', ]
+
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', ]
+    list_display_links = ['name', ]
+    search_fields = ['name', ]
+
+
+class UserModeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'quota']
+    list_filter = ['user', 'mode']
+
+
+class TrainingMaterialAdmin(admin.ModelAdmin):
+    list_display = ('title', 'updated_at', 'agree_text', 'numeration')
+    search_fields = ('title', 'description')
+    list_filter = ('created_at', 'updated_at')
+    list_editable = ('agree_text', 'numeration')
+
+
+admin.site.register(Mode, ModeAdmin)
+admin.site.register(Prompt, PromptAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(TrainingMaterial, TrainingMaterialAdmin)
+admin.site.register(UserMode, UserModeAdmin)
